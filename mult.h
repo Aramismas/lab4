8b97a07618cf44fd4c7d6cef7dda0b5ea3cc4793
arzhangv <avaladkhani14@gmail.com>
2020-11-04 12:56:43 -0800

    fixed the unit tests

--- mult.h
+++ mult.h
@@ -10,7 +10,7 @@ private:
 public:
         Mult(Base* num1 , Base* num2) : exp1(num1), exp2(num2), Base() { }
         virtual double evaluate(){ return exp1->evaluate()* exp2->evaluate();}
-        virtual std::string stringify(){return exp1->stringify() + "/" + exp2->stringify();}
+        virtual std::string stringify(){return exp1->stringify() + " * " + exp2->stringify();}
 };
 
 #endif
--- op_test.hpp
+++ op_test.hpp
@@ -1,3 +1,4 @@
+
 #ifndef __OP_TEST_HPP__
 #define __OP_TEST_HPP__
 
@@ -5,6 +6,10 @@
 
 #include "op.hpp"
 #include "add.h"
+#include "sub.h"
+#include "mult.h"
+#include "div.h"
+#include "pow.h"
 
 TEST(OpTest, OpEvaluateNonZero) {
     Op* test = new Op(8);
@@ -28,11 +33,106 @@ TEST(AddTest, AddEvaluatePositive) {
     EXPECT_EQ(test3->evaluate(), 5);
 }
 
+TEST(AddTest, AddEvaluateNegPos) {
+    Op* test1 = new Op(-2);
+    Op* test2 = new Op(3);
+    Add* test3 = new Add(test1, test2);
+    EXPECT_EQ(test3->evaluate(), 1);
+}
+
+TEST(AddTest, AddEvaluatePosNeg) {
+    Op* test1 = new Op(2);
+    Op* test2 = new Op(-3);
+    Add* test3 = new Add(test1, test2);
+    EXPECT_EQ(test3->evaluate(), -1);
+}
+
 TEST(AddTest, AddStringifyPositive) {
     Op* test1 = new Op(2);
     Op* test2 = new Op(3);
     Add* test3 = new Add(test1, test2);
     EXPECT_EQ(test3->stringify(), "2.000000 + 3.000000");
+}
+
+TEST(SubTest, SubStringifyPositive) {
+    Op* test1 = new Op(2);
+    Op* test2 = new Op(3);
+    Sub* test3 = new Sub(test1, test2);
+    EXPECT_EQ(test3->stringify(), "2.000000 - 3.000000");
+}
+
+TEST(MultTest, MultStringifyPositive) {
+    Op* test1 = new Op(8);
+    Op* test2 = new Op(7);
+    Mult* test3 = new Mult(test1, test2);
+    EXPECT_EQ(test3->stringify(), "8.000000 * 7.000000");
+}
+
+TEST(DivTest, DivStringifyPositive) {
+    Op* test1 = new Op(4);
+    Op* test2 = new Op(7);
+    Div* test3 = new Div(test1, test2);
+    EXPECT_EQ(test3->stringify(), "4.000000/7.000000");
+}
+
+TEST(PowTest, PowStringifyPositive) {
+   Op* test1 = new Op(2);
+    Op* test2 = new Op(3);
+    Pow* test3 = new Pow(test1, test2);
+    EXPECT_EQ(test3->stringify(), "2.000000**3.000000");
+}
+TEST(AddTest, AddStringifyNegative) {
+    Op* test1 = new Op(2);
+    Op* test2 = new Op(-3);
+    Add* test3 = new Add(test1, test2);
+    EXPECT_EQ(test3->stringify(), "2.000000 + -3.000000");
+}
+
+TEST(SubTest1, SubEvaluatePositive) {
+    Op* test1 = new Op(3);
+    Op* test2 = new Op(2);
+    Sub* test3 = new Sub(test1, test2);
+    EXPECT_EQ(test3->evaluate(), 1);
+}
+
+TEST(SubTest2, SubEvaluateNegative) {
+    Op* test1 = new Op(-3);
+    Op* test2 = new Op(2);
+    Sub* test3 = new Sub(test1, test2);
+    EXPECT_EQ(test3->evaluate(), -5);
+}
+
+TEST(SubTest3, SubStringifyPositive) {
+    Op* test1 = new Op(3);
+    Op* test2 = new Op(2);
+    Sub* test3 = new Sub(test1, test2);
+    EXPECT_EQ(test3->stringify(), "3.000000 - 2.000000");
+}
+
+TEST(SubTest4, SubStringifyNegative) {
+    Op* test1 = new Op(-2);
+    Op* test2 = new Op(3);
+    Sub* test3 = new Sub(test1, test2);
+    EXPECT_EQ(test3->stringify(), "-2.000000 - 3.000000");
+}
+
+TEST(MultTest, MultEvalPositive) {
+    Op* test1 = new Op(8);
+    Op* test2 = new Op(7);
+    Mult* test3 = new Mult(test1, test2);
+    EXPECT_EQ(test3->evaluate(), 56);
+}
+TEST(PowTest, PowEval) {
+    Op* test1 = new Op(2);
+    Op* test2 = new Op(3);
+    Pow* test3 = new Pow(test1, test2);
+    EXPECT_EQ(test3->evaluate(), 8);
+}
+TEST(DivTest, DivEvalPositive) {
+    Op* test1 = new Op(7);
+    Op* test2 = new Op(7);
+    Div* test3 = new Div(test1, test2);
+    EXPECT_EQ(test3->evaluate(),1 );
 }
 
 #endif //__OP_TEST_HPP__
