ab08baaa370a846c267499973ef6936b1ab24673
arzhangv <avaladkhani14@gmail.com>
2020-11-05 17:39:46 -0800

    Added a couple unit tests

--- op_test.hpp
+++ op_test.hpp
@@ -115,6 +115,12 @@ TEST(SubTest4, SubStringifyNegative) {
     Sub* test3 = new Sub(test1, test2);
     EXPECT_EQ(test3->stringify(), "-2.000000 - 3.000000");
 }
+TEST(MultTest, CheckZero) {
+    Op* test1 = new Op(8);
+    Op* test2 = new Op(0);
+    Mult* test3 = new Mult(test1, test2);
+    EXPECT_EQ(test3->evaluate(), 0);
+}
 
 TEST(MultTest, MultEvalPositive) {
     Op* test1 = new Op(8);
@@ -128,6 +134,15 @@ TEST(PowTest, PowEval) {
     Pow* test3 = new Pow(test1, test2);
     EXPECT_EQ(test3->evaluate(), 8);
 }
+
+TEST(PowTest, PowEvaltoTheZero) {
+    Op* test1 = new Op(2);
+    Op* test2 = new Op(0);
+    Pow* test3 = new Pow(test1, test2);
+    EXPECT_EQ(test3->evaluate(), 1);
+}
+
+
 TEST(DivTest, DivEvalPositive) {
     Op* test1 = new Op(7);
     Op* test2 = new Op(7);
@@ -135,4 +150,11 @@ TEST(DivTest, DivEvalPositive) {
     EXPECT_EQ(test3->evaluate(),1 );
 }
 
+
+TEST(DivTest, DivEvalNeg) {
+    Op* test1 = new Op(7);
+    Op* test2 = new Op(-7);
+    Div* test3 = new Div(test1, test2);
+    EXPECT_EQ(test3->evaluate(),-1 );
+}
 #endif //__OP_TEST_HPP__
