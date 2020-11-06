5c0ba4c774195c85f286bef2b768fb0e8b50d362
aramismas <aramismasarati@yahoo.com>
2020-11-05 23:29:23 -0800

    Updated Unit Tests

--- op_test.hpp
+++ op_test.hpp
@@ -10,6 +10,7 @@
 #include "mult.h"
 #include "div.h"
 #include "pow.h"
+#include "rand.h"
 
 TEST(OpTest, OpEvaluateNonZero) {
     Op* test = new Op(8);
@@ -84,7 +85,7 @@ TEST(PowTest, PowStringifyPositive) {
     Op* test1 = new Op(2);
     Op* test2 = new Op(3);
     Pow* test3 = new Pow(test1, test2);
-    EXPECT_EQ(test3->stringify(), "2.000000**3.000000");
+    EXPECT_EQ(test3->stringify(), "2.000000 ** 3.000000");
 }
 
 TEST(AddTest, AddStringifyNegative) {
@@ -126,6 +127,13 @@ TEST(MultTest, CheckZero) {
     Op* test2 = new Op(0);
     Mult* test3 = new Mult(test1, test2);
     EXPECT_EQ(test3->evaluate(), 0);
+} 
+
+TEST(MultTest, MultStringZero) {
+    Op* test1 = new Op(8);
+    Op* test2 = new Op(0);
+    Mult* test3 = new Mult(test1, test2);
+    EXPECT_EQ(test3->stringify(), "8.000000 * 0.000000");
 }
 
 TEST(MultTest, MultEvalPositive) {
@@ -134,6 +142,7 @@ TEST(MultTest, MultEvalPositive) {
     Mult* test3 = new Mult(test1, test2);
     EXPECT_EQ(test3->evaluate(), 56);
 }
+
 TEST(PowTest, PowEval) {
     Op* test1 = new Op(2);
     Op* test2 = new Op(3);
@@ -148,6 +157,12 @@ TEST(PowTest, PowEvaltoTheZero) {
     EXPECT_EQ(test3->evaluate(), 1);
 }
 
+TEST(PowTest, PowStringZero) {
+    Op* test1 = new Op(2);
+    Op* test2 = new Op(0);
+    Pow* test3 = new Pow(test1, test2);
+    EXPECT_EQ(test3->stringify(), "2.000000 ** 0.000000");
+}
 
 TEST(DivTest, DivEvalPositive) {
     Op* test1 = new Op(7);
@@ -161,6 +176,70 @@ TEST(DivTest, DivEvalNeg) {
     Op* test1 = new Op(7);
     Op* test2 = new Op(-7);
     Div* test3 = new Div(test1, test2);
-    EXPECT_EQ(test3->evaluate(),-1 );
+    EXPECT_EQ(test3->evaluate(), -1);
+}
+
+TEST(DivTest, DivStringNeg) {
+    Op* test1 = new Op(7);
+    Op* test2 = new Op(-7);
+    Div* test3 = new Div(test1, test2);
+    EXPECT_EQ(test3->stringify(), "7.000000 / -7.000000");
+}
+
+TEST(RandTest, RandEval) {
+    Rand* test = new Rand();
+    double test2 = test->evaluate();
+    EXPECT_EQ(test->evaluate(), test2);
+}
+
+TEST(RandTest, RandString) {
+    Rand* test = new Rand();
+    double test2 = test->evaluate();
+    std::string test3 = std::to_string(test2);
+    EXPECT_EQ(test->stringify(), test3);
+}
+
+TEST(ComboTest, ComboOneEval) {
+    Op* test1 = new Op(12);
+    Op* test2 = new Op(3);
+    Op* test3 = new Op(2);
+    Op* test4 = new Op(4);
+    Div* test5 = new Div(test1, test2);
+    Sub* test6 = new Sub(test5, test3); 
+    Pow* test7 = new Pow(test6, test4);
+    EXPECT_EQ(test7->evaluate(), 16);
+} 
+
+TEST(ComboTest, ComboOneString) {
+    Op* test1 = new Op(12);
+    Op* test2 = new Op(3);
+    Op* test3 = new Op(2);
+    Op* test4 = new Op(4);
+    Div* test5 = new Div(test1, test2);
+    Sub* test6 = new Sub(test5, test3);
+    Pow* test7 = new Pow(test6, test4);
+    EXPECT_EQ(test7->stringify(), "12.000000 / 3.000000 - 2.000000 ** 4.000000");
+} 
+
+TEST(ComboTest, ComboTwoEval) {
+    Op* test1 = new Op(2);
+    Op* test2 = new Op(3);
+    Op* test3 = new Op(4);
+    Op* test4 = new Op(2);
+    Add* test5 = new Add(test1, test2);
+    Mult* test6 = new Mult(test5, test3);
+    Pow* test7 = new Pow(test6, test4);
+    EXPECT_EQ(test7->evaluate(), 400);
+}
+
+TEST(ComboTest, ComboTwoString) {
+    Op* test1 = new Op(2);
+    Op* test2 = new Op(3);
+    Op* test3 = new Op(4);
+    Op* test4 = new Op(2);
+    Add* test5 = new Add(test1, test2);
+    Mult* test6 = new Mult(test5, test3);
+    Pow* test7 = new Pow(test6, test4);
+    EXPECT_EQ(test7->stringify(), "2.000000 + 3.000000 * 4.000000 ** 2.000000");
 }
 #endif //__OP_TEST_HPP__
